411ed48c20778a78c67b7caddb1266caa32f85e6
vstasch <vanessastasch1@hotmail.de>
2021-01-12 18:07:55 +0100

    token.java

--- src/de/ukoeln/idh/teaching/tri/ex08/Token.java
+++ src/de/ukoeln/idh/teaching/tri/ex08/Token.java
@@ -1,44 +1,25 @@
-package de.ukoeln.idh.teaching.tri.ex08;
-
 public class Token extends Annotation {
 	String pos = null;
 	String lemma = null;
+	int dep = 0;
 
 	public Token(Document document, int begin, int end, String pos, String lemma) {
 		super(document, begin, end);
 		this.pos = pos;
 		this.lemma = lemma;
+		this.dep = dep;
 	}
 
 	public Token getGovernor() {
 		// TODO: Implement me (step 3)
+		if (dep < 0) {
 		return null;
 	}
+
+	return document.getTokens().get(dep);
 
 	public String getRelation() {
 		// TODO: Implement me (step 3)
-		return null;
+		return getPos();
 	}
 
-	@Override
-	public String toString() {
-		return getDocument().getText().substring(getBegin(), getEnd());
-	}
-
-	public String getPos() {
-		return pos;
-	}
-
-	public void setPos(String pos) {
-		this.pos = pos;
-	}
-
-	public String getLemma() {
-		return lemma;
-	}
-
-	public void setLemma(String lemma) {
-		this.lemma = lemma;
-	}
-
-}
