94234d3123e440cf5b95e5cf36f63d8d75ee90fc
Kerstin <kerstin.vankan@web.de>
2021-01-12 13:01:34 +0100

    Token.java

--- src/de/ukoeln/idh/teaching/tri/ex08/Token.java
+++ src/de/ukoeln/idh/teaching/tri/ex08/Token.java
@@ -3,21 +3,26 @@ package de.ukoeln.idh.teaching.tri.ex08;
 public class Token extends Annotation {
 	String pos = null;
 	String lemma = null;
+	int dep = 0;
 
 	public Token(Document document, int begin, int end, String pos, String lemma) {
 		super(document, begin, end);
 		this.pos = pos;
 		this.lemma = lemma;
+		this.dep = dep;
 	}
 
 	public Token getGovernor() {
 		// TODO: Implement me (step 3)
+		if (dep < 0) {
 		return null;
 	}
 
+	return document.getTokens().get(dep);
+
 	public String getRelation() {
 		// TODO: Implement me (step 3)
-		return null;
+		return getPos();
 	}
 
 	@Override
