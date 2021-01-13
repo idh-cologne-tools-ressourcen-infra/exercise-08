5a882fdcd1bec67e3fed1fae32166b7a2ef9da3a
Lena <kislitcyn.lena@gmail.com>
2021-01-14 00:16:09 +0100

    Update NamedEntity.java

--- src/de/ukoeln/idh/teaching/tri/ex08/NamedEntity.java
+++ src/de/ukoeln/idh/teaching/tri/ex08/NamedEntity.java
@@ -1,19 +1,23 @@
   package de.ukoeln.idh.teaching.tri.ex08;
+  import java.util.List;
+  import.java.util.HashMap;
+  import.java.util.ArrayList;
+  import.java.util.Map;
 
 public class NamedEntity {
-	Token token;
-	String type;
-	float prob;
-	
-	public NamedEntity(Token token, String type, float prob){
-		this.token = token;
-		this.type = type;
-		this.prob = prob;
-	}
-	
-	@Override
-	public String toString() {
-	return type;
-	}
+	private Map<String, List<Token>> namedEntities = new HashMap<String, List<Token>>(); 
+	private List<Token> nameEntityTokens = new ArrayList<Token>(); 
+	 
+	public void saveNamedEntityTokens(Token token) { 
+		nameEntityTokens.add(token); 
+	} 
+	 
+	public void saveNamedEntity() { 
+		namedEntities.put("person", nameEntityTokens); 
+	} 
+	 
+	public Map<String, List<Token>> getNamedEntities() { 
+		return namedEntities; 
+	} 
 	
 }
