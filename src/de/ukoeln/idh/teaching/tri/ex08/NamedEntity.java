186e7d9067b3fd11cfaafa61dca9b487bd451c1c
kekpja <56304716+kekpja@users.noreply.github.com>
2021-02-15 15:33:21 +0100

    Added Category/Confidence + getters/setters

--- src/de/ukoeln/idh/teaching/tri/ex08/NamedEntity.java
+++ src/de/ukoeln/idh/teaching/tri/ex08/NamedEntity.java
@@ -1,5 +1,29 @@
 package de.ukoeln.idh.teaching.tri.ex08;
 
-public class NamedEntity {
+public class NamedEntity extends Annotation {
+
+	String category = null;
+	double confidence = 0;
+
+	public NamedEntity(Document document, int begin, int end) {
+		super(document, begin, end);
+		// TODO Auto-generated constructor stub
+	}
+
+	public String getCategory() {
+		return category;
+	}
+
+	public void setCategory(String cat) {
+		this.category = cat;
+	}
+
+	public double getConfidence() {
+		return confidence;
+	}
+
+	public void setConfidence(double con) {
+		this.confidence = con;
+	}
 
 }
